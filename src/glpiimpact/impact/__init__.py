0894b25828f9bbb13e903a92bade3de169e772de
erodriguezgaleas <edmarce27@gmail.com>
2026-09-17 10:36:24 -0500

    feat(impact): expose builder importer and exporter

--- src/glpiimpact/impact/__init__.py
+++ src/glpiimpact/impact/__init__.py
@@ -1,5 +1,15 @@
 """GLPI Impact graph modelling and browser automation."""
 
+from .builder import ImpactBuilder
+from .exporter import ImpactExporter
 from .graph import ImpactEdge, ImpactGraph, ImpactNode
+from .importer import ImpactImporter
 
-__all__ = ["ImpactNode", "ImpactEdge", "ImpactGraph"]
+__all__ = [
+    "ImpactNode",
+    "ImpactEdge",
+    "ImpactGraph",
+    "ImpactBuilder",
+    "ImpactExporter",
+    "ImpactImporter",
+]

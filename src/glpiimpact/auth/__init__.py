c201ea51f6152ebcd6ea2b426ab6471a5c9d39a5
erodriguezgaleas <edmarce27@gmail.com>
2026-09-17 10:55:14 -0500

    feat(auth): expose authentication helpers

--- src/glpiimpact/auth/__init__.py
+++ src/glpiimpact/auth/__init__.py
@@ -1,5 +1,13 @@
 """Authentication package for GLPI Impact Builder."""
 
+from .cookies import CookieManager
+from .csrf import CsrfManager
 from .login import LoginManager
+from .session import AuthenticatedSession
 
-__all__ = ["LoginManager"]
+__all__ = [
+    "LoginManager",
+    "CsrfManager",
+    "CookieManager",
+    "AuthenticatedSession",
+]
